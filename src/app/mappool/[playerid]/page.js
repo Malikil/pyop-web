3c97a3c1f5b9d1efb60d7a393c66ef3ff0be47aa
Eric Hagen <malikerkagen@gmail.com>
2025-03-10 23:22:04 -0700

    Individual player pools for approvers

--- src/app/mappool/[playerid]/page.js
+++ src/app/mappool/[playerid]/page.js
@@ -1,105 +1,56 @@
-"use client";
-
 import { Container, Spinner } from "react-bootstrap";
 import { ModsEnum } from "osu-web.js";
-import usePlayer from "@/hooks/usePlayer";
-import { useRouter } from "next/navigation";
+import { redirect } from "next/navigation";
 import PoolStats from "@/components/mappool/PoolStats";
-import { useEffect, useState } from "react";
 import MapList from "@/components/mappool/MapList";
-import { useSWRConfig } from "swr";
-import AddMapButton from "./AddMapButton";
+import db from "@/app/api/db/connection";
+import { checkApprover } from "@/app/admin/approver/actions";
+import { auth } from "@/auth";
 
-export default function Mappool() {
-   const { mutate } = useSWRConfig();
-   const { data: player, isLoading, isError } = usePlayer();
-   const router = useRouter();
+export default async function Mappool({ params }) {
+   const session = await auth();
+   if (!session || !(await checkApprover(session.user.id))) redirect("/");
+   const playersCollection = db.collection("players");
+   const player = await playersCollection.findOne(
+      { osuid: parseInt(params.playerid) },
+      { projection: { _id: 0, "maps.current.screenshot": 0 } }
+   );
+   if (!player) redirect("/admin/approver");
 
-   const [maps, setMaps] = useState({
-      nm: [],
-      hd: [],
-      hr: [],
-      dt: [],
-      other: []
-   });
-   useEffect(() => {
-      if (isError || isLoading) return;
-      setMaps(
-         player.maps.current.reduce(
-            (sorted, map) => {
-               switch (map.mods) {
-                  case 0:
-                     sorted.nm.push(map);
-                     break;
-                  case ModsEnum.HD:
-                     sorted.hd.push(map);
-                     break;
-                  case ModsEnum.HR:
-                     sorted.hr.push(map);
-                     break;
-                  case ModsEnum.DT:
-                     sorted.dt.push(map);
-                     break;
-                  default:
-                     sorted.other.push(map);
-               }
-               return sorted;
-            },
-            {
-               nm: [],
-               hd: [],
-               hr: [],
-               dt: [],
-               other: []
-            }
-         )
-      );
-   }, [player, isLoading, isError]);
+   const maps = player.maps.current.reduce(
+      (sorted, map) => {
+         switch (map.mods) {
+            case 0:
+               sorted.nm.push(map);
+               break;
+            case ModsEnum.HD:
+               sorted.hd.push(map);
+               break;
+            case ModsEnum.HR:
+               sorted.hr.push(map);
+               break;
+            case ModsEnum.DT:
+               sorted.dt.push(map);
+               break;
+            default:
+               sorted.other.push(map);
+         }
+         return sorted;
+      },
+      {
+         nm: [],
+         hd: [],
+         hr: [],
+         dt: [],
+         other: []
+      }
+   );
 
-   if (isLoading) return <Spinner className="m-4" />;
-   if (isError || !player) return router.push("/");
    return (
       <Container className="py-2">
-         <MapList
-            maps={maps}
-            mapActions={[
-               {
-                  title: "Remove",
-                  action: beatmap =>
-                     mutate(
-                        "/api/db/player",
-                        () =>
-                           fetch(`/api/db/maps?id=${beatmap.id}&mods=${beatmap.mods}`, {
-                              method: "DELETE"
-                           }).then(res => res.json()),
-                        {
-                           optimisticData: player => {
-                              const index = player.maps.current.findIndex(
-                                 m => m.id === beatmap.id && m.mods === beatmap.mods
-                              );
-                              return {
-                                 ...player,
-                                 maps: {
-                                    ...player.maps,
-                                    current: player.maps.current.filter((_, i) => i !== index)
-                                 }
-                              };
-                           },
-                           populateCache: (result, player) => ({
-                              ...player,
-                              maps: {
-                                 ...player.maps,
-                                 current: result
-                              }
-                           })
-                           //revalidate: true
-                        }
-                     )
-               }
-            ]}
-         />
+         <h1>Pool for {player.osuname}</h1>
+         <MapList maps={maps} />
          <PoolStats maps={player.maps.current} />
-         <AddMapButton count={player.maps.current.length} />
       </Container>
    );
 }
